d71323765b1507b242d95db8d81cd54a20d1f5d1
Roman_B <roman.bichukin@gmail.com>
2022-09-24 12:31:20 +0900

    метод заполнения массива

---
+++ lesson___2/generator/Program.cs
@@ -0,0 +1,32 @@
+﻿//переписывам предыдущий код с использование генератора
+//случайных чисел
+//
+void FillArray(int[] collection)        //заполняем массив случайными числами
+{
+    int length = collection.Length;  //получаем длину массива
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);   //заполняем "collection" случайными
+        index++;    // index = index + 1                  числами от "1"  до "10"
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];    //в массиве array по умолчанию, будет 10 элементов.
+                            //по умолчанию, заполнится нулями.
+
+FillArray(array);         //          
+PrintArray(array);        //
+
+
